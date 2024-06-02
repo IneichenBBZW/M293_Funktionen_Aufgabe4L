138a950f48ef0161b8420fee1812179456a3caa6
IneichenBBZW <98276716+IneichenBBZW@users.noreply.github.com>
2024-06-02 13:59:29 +0200

    Add files via upload

--- js/src/ArrayHelper.js
+++ js/src/ArrayHelper.js
@@ -3,17 +3,20 @@
 bei der Definition von Objektmethoden das Schlüsselwort function und den 
 vorausgehenden Doppelpunkt auch weglassen. */
 const ArrayHelper = {
-  summe(array) {
-    return array.reduce((sum, value) => sum + value, 0);
-  },
-  produkt(array) {
-    return array.reduce((produkt, value) => produkt * value, 1);
-  },
-  summeEasy: function (array) {
+  summe: function(array) {
     let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-      sum += array[i]; // sum = sum + array[i];
+    for (let i = 0; i < array.length; i++){
+      sum += array[i];
     }
     return sum;
+    // return array.reduce((sum, value) => sum + value, 0);
+  },
+  produkt(array) {
+    let prod = 1;
+    for (let i = 0; i < array.length; i++){
+      prod *= array[i];
+    }
+    return prod;
+    // return array.reduce((prod, value) => prod * value, 1);
   },
 };
